34e30da03556ab9eced8d8a95935f0ad88b9a065
Aashay <arkhamknight.aashay@gmail.com>
2023-07-18 00:11:59 +0530

    added menu card

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,25 @@ import axios from 'axios'
 import Link from 'next/link'
 
 export default function Home() {
-  const getImage = () => {
+  const getTestData = () => {
     axios.get('https://basic-node-lx95.onrender.com/test')
       .then((res) => {
         console.log("response for image:", res)
       })
       .catch(err => console.log("error in getting image", err))
   }
+
   return (
     <div>
-      broo
-      <button onClick={() => getImage()}>get my image</button>
-      <button onClick={() => alert('you have used alert')}>click me </button>
+      <div className='pb-10'>
+        <button onClick={() => getTestData()}>get test Data</button>
+        <button onClick={() => alert('you have used alert')}>click me </button>
+      </div>
+      <div>
+
+        {/* <button onClick={() => alert('you have used alert')}>Get Menu Card </button> */}
+        <Link className='border rounded-lg p-4 hover:bg-green-500' href={"https://basic-node-lx95.onrender.com/getMenuCard"}>Get Menu Card</Link>
+      </div>
     </div>
   )
 }
