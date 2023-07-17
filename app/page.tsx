f9b730d2dfb54843727ca35c52a2dc01e0d91dfd
Aashay <arkhamknight.aashay@gmail.com>
2023-07-18 00:35:06 +0530

    added fetching data

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import axios from 'axios'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+  const [users, setUsers] = useState<any[]>([])
   const getTestData = () => {
     axios.get('https://basic-node-lx95.onrender.com/test')
       .then((res) => {
-        console.log("response for image:", res)
+        console.log("response for image:", res.data)
+        setUsers(res.data)
       })
       .catch(err => console.log("error in getting image", err))
   }
@@ -21,6 +24,12 @@ export default function Home() {
 
         {/* <button onClick={() => alert('you have used alert')}>Get Menu Card </button> */}
         <Link className='border rounded-lg p-4 hover:bg-green-500' href={"https://basic-node-lx95.onrender.com/getMenuCard"}>Get Menu Card</Link>
+      </div>
+
+      <div>
+        {
+          users.map(user => <div>{user.id} {user.name} {user.email}</div>)
+        }
       </div>
     </div>
   )
